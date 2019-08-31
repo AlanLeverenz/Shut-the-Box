6a5bc17876028e874af044ffe3e3fe0cbf88d417
Alan Leverenz <awleverenz@aol.com>
2019-08-31 01:56:49 -0400

    updated the html presentation

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+$(document).ready(function() { 
+
+// $("#one-dice").hide();
+
+$("#6").css("background-color","brown");
+$("#6").css("color","orange");
+
+// on click on throwing dice, fade in the dice...
+// blink on the available tabs to click on, i.e., equal to one or both dice.
+// when user clicks on one, it changes colors and is not clickable until next turn
+// if 7, 8, 9 taken, make one dice available to throw
+
+
+
+});
